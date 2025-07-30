fa9404b9b514dad9eccb3e1cebafaeac4b7e466a
Bive17 <134941496+Bive17@users.noreply.github.com>
2025-07-30 22:26:24 +0700

    Delete Ngày em đẹp nhất directory

--- Ngày em đẹp nhất/TechnicalSupportApi/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-namespace TechnicalSupportApi.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string DisplayName { get; set; }
-        public string? Expertise { get; set; } // For technicians (e.g., "Hardware", "Software")
-    }
-}
--- Ngày em đẹp nhất/TechnicalSupportApi/TicketHub.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-namespace TechnicalSupportApi
-{
-    public class TicketHub : Hub
-    {
-        // Có thể để trống hoặc bổ sung các method realtime sau
-    }
-} 
